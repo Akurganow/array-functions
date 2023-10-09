f11d1c7f614141ac5983f1183bf30617460c82fe
Alexander Kurganov <a.kurganow@gmail.com>
2023-10-09 22:54:03 +0400

    fix: export Sortable types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { isObject } from '@plq/is'
 
-type Sortable<T> = { [k in keyof T]: string | number | (() => string | number) }
-type SortableKey<T> = T extends Sortable<T> ? keyof T : never
-type SortableOrder = 'asc' | 'desc'
+export type Sortable<T> = { [k in keyof T]: string | number | (() => string | number) }
+export type SortableKey<T> = T extends Sortable<T> ? keyof T : never
+export type SortableOrder = 'asc' | 'desc'
 
 const defaultOrder: SortableOrder = 'desc'
 
