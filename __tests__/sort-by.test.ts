10b123376268669fa77292916bdfb8e07a2bc03a
Alexander Kurganov <a.kurganow@gmail.com>
2023-10-10 22:52:43 +0400

    chore: Add negative tests for sortBy

--- __tests__/sort-by.test.ts
+++ __tests__/sort-by.test.ts
@@ -1,6 +1,15 @@
 import { sortBy, getKeyValue } from '../src'
 
 describe('sortBy', () => {
+	test('empty', () => {
+		const items: Array<{ id: string }> = []
+
+		const sortedAsc = sortBy(items, 'id', 'asc')
+		const sortedDesc = sortBy(items, 'id', 'desc')
+
+		expect(sortedAsc).toEqual([])
+		expect(sortedDesc).toEqual([])
+	})
 	test('string', () => {
 		const items = [{ id: 'b' }, { id: 'c' }, { id: 'a' }]
 
@@ -40,5 +49,17 @@ describe('sortBy', () => {
 
 		expect(sortedAsc).toEqual([1, 2, 3])
 		expect(sortedDesc).toEqual([3, 2, 1])
+	})
+	test('mixed', () => {
+		const items = [{ id: 'b' }, { id: 3 }, { id: 'a' }, { id: 2 }]
+
+		expect(() => { sortBy(items, 'id') }).toThrow()
+	})
+	test('not a string or number', () => {
+		const items = [{ id: {} }, { id: {} }]
+
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		expect(sortBy(items, 'id')).toEqual(items)
 	})
 })
