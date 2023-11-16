1780bb581fd89d42403cdede8408379b6f90eb35
Alexander Kurganov <a.kurganow@gmail.com>
2023-11-16 21:07:33 +0400

    chore: Add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,45 @@ export type SortableOrder = 'asc' | 'desc'
 
 const defaultOrder: SortableOrder = 'desc'
 
-export function filterBySameKeyValue<T extends { [k in string]: unknown }>(value: T, index: number, array: T[], key: keyof T) {
+/**
+ * Filters an array of objects so that the value of a given key occurs only once in the array.
+ * @template T The type of the object.
+ * @param {T} value The current value.
+ * @param {number} index The index of the current value.
+ * @param {T[]} array The array of objects.
+ * @param {keyof T} key The key to filter by.
+ * @returns {boolean} Whether the value is the first occurrence of the key's value in the array.
+ *
+ * @example
+ * const array = [{ id: 1 }, { id: 2 }, { id: 1 }];
+ * const filtered = array.filter((value, index, array) => filterBySameKeyValue(value, index, array, 'id'));
+ * // filtered: [{ id: 1 }, { id: 2 }]
+ */
+export function filterBySameKeyValue<T extends { [k in string]: unknown }>(value: T, index: number, array: T[], key: keyof T): boolean {
 	return index === array.findIndex(v => v[key] === value[key])
 }
 
+/**
+ * Compares two values.
+ * @param {string | number} a The first value.
+ * @param {string | number} b The second value.
+ * @param {SortableOrder} [order='desc'] The order of sorting.
+ * @returns {number} The result of the comparison.
+ *
+ * This function compares two values `a` and `b`. Depending on the type of these values, the function uses different comparison approaches:
+ * - If the values are strings, the function uses the `localeCompare` method, which compares strings based on locale. This means that the strings are compared according to the sorting rules for the current language and region.
+ * - If the values are numbers, the function simply subtracts one number from another. This gives a negative number if `a` is less than `b`, a positive number if `a` is greater than `b`, and zero if `a` and `b` are equal.
+ * - If the values are of any other type, the function returns `0`.
+ *
+ * The function also takes into account the sorting order, which can be either 'asc' (ascending) or 'desc' (descending). If the sorting order is 'asc', the function returns the comparison result as is. If the sorting order is 'desc', the function returns the reverse comparison result.
+ *
+ * @example
+ * ['a', 'b'].sort((a, b) => compareValues(a, b, 'asc')); // Output: ['b', 'a']
+ */
 export function compareValues<T extends (string | number)>(a: T, b: T, order: SortableOrder = defaultOrder): number {
-	switch (typeof a) {
+	const type = typeof a
+
+	switch (type) {
 		case 'string':
 			return order === 'asc'
 				? (a as string).localeCompare(b as string)
@@ -25,8 +58,18 @@ export function compareValues<T extends (string | number)>(a: T, b: T, order: So
 	}
 }
 
-
-export function sortBy<T extends Sortable<T>>(items: T[], key: SortableKey<T>, order: SortableOrder = defaultOrder) {
+/**
+ * Sorts an array of objects by a given key.
+ * @template T The type of the object.
+ * @param {T[]} items The array of objects.
+ * @param {SortableKey<T>} key The key to sort by.
+ * @param {SortableOrder} [order='desc'] The order of sorting.
+ * @returns {T[]} The sorted array.
+ *
+ * @example
+ * sortBy([{ id: 2 }, { id: 1 }], 'id', 'asc'); // Output: [{ id: 1 }, { id: 2 }]
+ */
+export function sortBy<T extends Sortable<T>>(items: T[], key: SortableKey<T>, order: SortableOrder = defaultOrder): T[] {
 	if (items.length <= 1) return items
 
 	return items.sort((a, b) => {
@@ -44,6 +87,16 @@ export function sortBy<T extends Sortable<T>>(items: T[], key: SortableKey<T>, o
 	})
 }
 
+/**
+ * Checks if an array of values is sorted.
+ * @template T The type of the values.
+ * @param {T[]} values The array of values.
+ * @param {SortableOrder} [order='desc'] The order of sorting.
+ * @returns {boolean} Whether the array is sorted.
+ *
+ * @example
+ * isSortedValues([1, 2, 3], 'asc'); // Output: true
+ */
 export function isSortedValues<T extends (string | number)>(values: T[], order: SortableOrder = defaultOrder): boolean {
 	if (values.length === 0) return true
 
@@ -67,7 +120,18 @@ export function isSortedValues<T extends (string | number)>(values: T[], order: 
 	})
 }
 
-export function isSortedBy<T extends Sortable<T>>(array: T[], key: SortableKey<T>, order: SortableOrder = defaultOrder) {
+/**
+ * Checks if an array of objects is sorted by a given key.
+ * @template T The type of the object.
+ * @param {T[]} array The array of objects.
+ * @param {SortableKey<T>} key The key to check by.
+ * @param {SortableOrder} [order='desc'] The order of sorting.
+ * @returns {boolean} Whether the array is sorted by the key.
+ *
+ * @example
+ * isSortedBy([{ id: 1 }, { id: 2 }], 'id', 'asc'); // Output: true
+ */
+export function isSortedBy<T extends Sortable<T>>(array: T[], key: SortableKey<T>, order: SortableOrder = defaultOrder): boolean {
 	if (array.length <= 1) return true
 
 	if (!isObject(array[0])) throw new Error(`Array is not an array of objects. (${typeof array[0]})`)
@@ -84,12 +148,35 @@ export function isSortedBy<T extends Sortable<T>>(array: T[], key: SortableKey<T
 	return isSortedValues(mapped, order)
 }
 
+/**
+ * Returns the array of values of a given key from an array of objects.
+ * @template T The type of the object.
+ * @param {T[]} arr The array of objects.
+ * @param {keyof T} key The key to get values by.
+ * @returns {T[keyof T][]} The array of values.
+ *
+ * @example
+ * const array = [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }];
+ * getKeyValue(array, 'name'); // Output: ['Alice', 'Bob']
+ */
 export function getKeyValue<T extends { [k in string]: unknown }>(arr: T[], key: keyof T): T[keyof T][] {
 	return arr.map(item => item[key])
 }
 
+/**
+ * Returns an array of unique values from an array of objects.
+ * @template T The type of the object.
+ * @param {T[]} arr The array of objects.
+ * @param {keyof T} key The key to get unique values by.
+ * @returns {T[keyof T][]} The array of unique values.
+ *
+ * @example
+ * const array = [{ id: 1, name: 'Alice' }, { id: 2, name: 'Alice' }];
+ * getUniqueValues(array, 'name'); // Output: ['Alice']
+ */
 export function getUniqueValues<T extends { [k in string]: unknown }>(arr: T[], key: keyof T): T[keyof T][] {
-	const filtered = arr.filter((item, index, array) => filterBySameKeyValue(item, index, array, key))
+	const uniqueValues = new Set<T[keyof T]>()
+	arr.forEach(item => uniqueValues.add(item[key]))
 
-	return getKeyValue(filtered, key)
+	return Array.from(uniqueValues)
 }
